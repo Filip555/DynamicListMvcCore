d170fa619b40a8fd9a29db8cec060109bdaa467c
Filipek Paweł <filipek@regis1.com.pl>
2017-03-30 19:09:33 +0200

    fc

---
+++ DynamicList/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DynamicList.Model;
+
+namespace DynamicList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Send(ICollection<SomeValue> someValue)
+        {
+            return RedirectToAction("Index");
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ DynamicList/Model/SomeValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicList.Model
+{
+    public class SomeValue
+    {
+        public string Name { get; set; }
+        public float Value { get; set; }
+    }
+}
